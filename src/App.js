1009e1e9d5bc39446d9c01df9db94c6d400bd9e3
cneemish <cneemish@gmail.com>
2025-04-08 22:18:26 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import { Alert, Breadcrumb, Button, Card } from 'react-bootstrap';
+import { Alert, Breadcrumb, Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container >
+        <Form>
+          <Row>
+            <Col md>
+          <Form.Group controlId='formEmail'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email' placeholder='example@example.com'/>
+            <Form.Text className='text-muted'>We won't use this email</Form.Text>
+          </Form.Group>
+          </Col>
+          <Col>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Password'/>
+          </Form.Group>
+          </Col>
+          </Row>
+        </Form>
       <Card className='mb-3' style={{color:'GrayText'}}>
-        <Card.Img src='https://picsum.photos/200'/>
+        <Card.Img src='https://picsum.photos/100'/>
         <Card.Body>
           <Card.Title>This is test  </Card.Title>
           <Card.Text>This is test Text</Card.Text>
@@ -23,7 +40,7 @@ function App() {
         </div>
         <Alert variant='primary'> This is a button</Alert>
         <Button variant='success'>Test Button</Button>
-      
+        </Container>
       </header>
     </div>
   );
