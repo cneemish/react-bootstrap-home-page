4ac8023c42fe7fc4ad349261840499966ff0209f
cneemish <cneemish@gmail.com>
2025-04-12 00:24:37 +0530

    Add useState

--- src/App.js
+++ src/App.js
@@ -27,6 +27,18 @@ function App() {
     changeNumber();
     changeColor();
   }
+
+  const [carbrand, setCarBrand] = useState("Tata");
+  const [carModel, setCarModel] = useState("Punch Kaziranag Edition");
+  const [carYear, setCarYear] = useState("2022");
+  const [carColor, setCarColor] = useState("Bage Green");
+
+  const [bike, setBike] = useState({
+    bikeBrand: "KTM",
+    bikeModel: "Adventure 390s",
+    bikeColor: "KTM Organe",
+    bikeYear: "2025"
+  });
     /* let color = "Red";
     const changeColor = ()=>{
       color = "Yellow";
@@ -59,7 +71,12 @@ function App() {
         <Card.Body>
           <Card.Title>This is test  </Card.Title>
           <Card.Text>This is test for number{number}</Card.Text>
-          <Card.Text> My car Brand</Card.Text>
+          <Card.Text> My car brand is {carbrand}</Card.Text>
+          <Card.Text>It is {carbrand} {carModel}</Card.Text>
+          <Card.Text>It was manufactured in {carYear}</Card.Text>
+          <Card.Text>The color of {carModel} is {carColor}</Card.Text>
+          <Card.Text>My bike brand is {bike.bikeBrand}</Card.Text>
+          <Card.Text>The model is {bike.bikeBrand} {bike.bikeModel} color of the bike is {bike.bikeColor} delivery starts from {bike.bikeYear}</Card.Text>
         </Card.Body>
       </Card>
       {/* <div>
